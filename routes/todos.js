e12c8a4de9464ca5989724a090d65014c13010a6
Ekemini <42721358+b4ssey@users.noreply.github.com>
2022-01-06 11:30:27 +0000

    added auth token to post todo request

--- routes/todos.js
+++ routes/todos.js
@@ -3,13 +3,14 @@ const { Todo, validate } = require("../models/todo");
 const router = express.Router();
 const mongoose = require("mongoose");
 const { User } = require("../models/user");
+const auth = require("../middleware/auth");
 
-router.get("/", async (req, res) => {
+router.get("/", auth, async (req, res) => {
   const todos = await Todo.find().sort("-dateChosen");
   res.send(todos);
 });
 
-router.post("/", async (req, res) => {
+router.post("/", auth, async (req, res) => {
   let { title, note, tag, oneTime, dateChosen, owner } = req.body;
   const { error } = validate(title, note, tag, oneTime, dateChosen, owner);
   if (error) return res.status(400).send(error.details[0].message);
