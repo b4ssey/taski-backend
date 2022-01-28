79bfc735654158580863f3dc4d7e6f28de659253
Ekemini <42721358+b4ssey@users.noreply.github.com>
2022-01-28 11:55:36 +0000

    a little test

--- startup/config.js
+++ startup/config.js
@@ -1,7 +1,7 @@
 const config = require("config");
 
 module.exports = function () {
-  console.log("na him", config.get("jwtPrivateKey"), config.get("pass"));
+  console.log("na him", config.get("jwtPrivateKey"), config.get("db"));
   if (!config.get("jwtPrivateKey")) {
     throw new Error("FATAL ERROR: jwtPrivateKey is not defined");
   }
